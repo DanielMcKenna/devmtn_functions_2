c1ae8f704d9e3552fc8d20aee46d9a91d52251ae
Daniel McKenna <mckenna.daniel15@gmail.com>
2021-10-12 14:07:34 -0600

    my callback is working but i honestly am not understanding how calling my function and not passing in a function is working to get the answer

--- callbackAndHigherOrder.js
+++ callbackAndHigherOrder.js
@@ -10,17 +10,13 @@ const multiply = (num1, num2, cb) => {
   cb(num1 * num2);
 };
 
-function callBack(num1) {
-  return num1 + 2;
-}
-
 // UNCOMMENT THE FUNCTION CALL BELOW
 // RUN THIS FILE WITH NODE
 // CHECK YOUR ANSWER
 
-// multiply(4, 3, answer => {
-//   console.log('The answer is ' + answer) //should console.log 12
-// })
+multiply(4, 3, (answer) => {
+  console.log("The answer is " + answer); //should console.log 12
+});
 
 ////////// PROBLEMS 2 - 6 //////////
 
